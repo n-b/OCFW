947febf01e0061fef36bcb963f15e5ba321482fc
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2012-03-06 23:28:41 +0100

    Added basic script for CocoaHTTPServer.

--- buildframeworks.sh
+++ buildframeworks.sh
@@ -1,82 +1,83 @@
 #!/bin/sh
 
-# Move to directory
-if [ $1 ]
+# check params
+if [ ! -n "$1" ]
 	then
-	if [ -d $1 ]
-		then
-		OLDWD=`pwd`
-		cd $1
-	else
-		echo "Usage : $0 [path/to/directory]"
-		exit -1
-	fi
+	echo "Usage : $0 [path/to/lib/directory]"
+	exit -1
 fi
 
+if [ ! -d $1 ]
+then
+	echo "$1 does not exist"
+	exit -2
+fi
+
+# move to target
+OLDWD=`pwd`
+cd $1
+
 # internal variables
-# PLATFORMS="macosx iphoneos iphonesimulator"
-# SHORT_PLATFORMS="macosx iphone"
-PLATFORMS="iphoneos iphonesimulator"
-SHORT_PLATFORMS="iphone"
+SDKS="macosx iphoneos iphonesimulator"
+PLATFORMS="macos iphone"
 
 SYMROOT="build"
 PUBLIC_HEADERS_FOLDER_PATH="Headers"
 
+XCODEPROJ_NAME="Framework.xcodeproj"
 LIBRARY_TARGET_NAME="Library"
 
-# find Xcode projects to compile
-PROJECTS="*.xcodeproj"
-if [ ! -d $PROJECTS ]
+# find Xcode project to compile
+PROJECTS=(*.xcodeproj)
+if [ ! -d ${XCODEPROJ_NAME} ]
 	then
-	echo "no Xcode project found in `pwd`"
+	echo "There must be a project named ${XCODEPROJ_NAME} in `pwd`"
 	exit -1
 fi
+PROJECT=${PROJECTS[0]}
 
-# For each Xcode project
-for PROJECT in $PROJECTS
+# found
+LIBRARY=$(basename $(pwd))
+echo "building ${LIBRARY}"
+
+# Compile
+for SDK in $SDKS
 do
-	LIBRARY=${PROJECT%.*}
-	echo "building ${LIBRARY}"
-
-    # Compile
-    for PLATFORM in $PLATFORMS
-    do
-		echo " compiling for ${PLATFORM}"
-        xcodebuild build\
-            -project $PROJECT\
-            -configuration Release\
-            -target ${LIBRARY_TARGET_NAME}\
-        	PRODUCT_NAME=${LIBRARY}\
-            SDKROOT=${PLATFORM}\
-            PUBLIC_HEADERS_FOLDER_PATH=${PUBLIC_HEADERS_FOLDER_PATH}\
-            SYMROOT=${SYMROOT}\
-			CONFIGURATION_BUILD_DIR=${SYMROOT}/${LIBRARY}.libraries/${PLATFORM}\
-            ARCHS=\$VALID_ARCHS\
-			> /tmp/$$.xcodebuild.log # Store log away and display it in case of error
-		if [ $? -ne 0 ]
-			then
-			cat /tmp/$$.xcodebuild.log
-			rm /tmp/$$.xcodebuild.log	
-			exit 1
-		fi
+	echo " compiling for ${SDK}"
+    xcodebuild build\
+        -project ${XCODEPROJ_NAME}\
+        -configuration Release\
+        -target ${LIBRARY_TARGET_NAME}\
+    	PRODUCT_NAME=${LIBRARY}\
+        SDKROOT=${SDK}\
+        PUBLIC_HEADERS_FOLDER_PATH=${PUBLIC_HEADERS_FOLDER_PATH}\
+        SYMROOT=${SYMROOT}\
+		CONFIGURATION_BUILD_DIR=${SYMROOT}/${LIBRARY}.libraries/${SDK}\
+        ARCHS=\$VALID_ARCHS\
+		> /tmp/$$.xcodebuild.log # Store log away and display it in case of error
+	if [ $? -ne 0 ]
+		then
+		cat /tmp/$$.xcodebuild.log
 		rm /tmp/$$.xcodebuild.log	
-    done
+		exit 1
+	fi
+	rm /tmp/$$.xcodebuild.log	
+done
 
-    # Make frameworks
-    for PLATFORM in $SHORT_PLATFORMS
-    do
-		echo " making framework for ${PLATFORM}"
-        FRAMEWORK_PATH="${SYMROOT}/${PLATFORM}/${LIBRARY}.framework"
-        mkdir -p "${FRAMEWORK_PATH}/Versions/A/Headers"
+# Make frameworks
+for PLATFORM in $PLATFORMS
+do
+	echo " making framework for ${PLATFORM}"
+    FRAMEWORK_PATH="${PLATFORM}/${LIBRARY}.framework"
+    mkdir -p "${FRAMEWORK_PATH}/Versions/A/Headers"
 
-        ln -sfh "A" "${FRAMEWORK_PATH}/Versions/Current"
-        ln -sfh "Versions/Current/Headers" "${FRAMEWORK_PATH}/Headers"
-        ln -sfh "Versions/Current/${LIBRARY}" "${FRAMEWORK_PATH}/${LIBRARY}"
+    ln -sfh "A" "${FRAMEWORK_PATH}/Versions/Current"
+    ln -sfh "Versions/Current/Headers" "${FRAMEWORK_PATH}/Headers"
+    ln -sfh "Versions/Current/${LIBRARY}" "${FRAMEWORK_PATH}/${LIBRARY}"
 
-        cp -a `find ${SYMROOT}/${LIBRARY}.libraries/${PLATFORM}*/${PUBLIC_HEADERS_FOLDER_PATH}/` "${FRAMEWORK_PATH}/Versions/A/Headers"
-        lipo -create `find ${SYMROOT}/${LIBRARY}.libraries/${PLATFORM}*/lib${LIBRARY}.a` -output "${FRAMEWORK_PATH}/Versions/Current/${LIBRARY}"
-		echo "  ${FRAMEWORK_PATH}"
-    done
+    cp -a `find ${SYMROOT}/${LIBRARY}.libraries/${PLATFORM}*/${PUBLIC_HEADERS_FOLDER_PATH}/` "${FRAMEWORK_PATH}/Versions/A/Headers"
+    lipo -create `find ${SYMROOT}/${LIBRARY}.libraries/${PLATFORM}*/lib${LIBRARY}.a` -output "${FRAMEWORK_PATH}/Versions/Current/${LIBRARY}"
+	echo "  ${FRAMEWORK_PATH}"
 done
 
 # cleanup
