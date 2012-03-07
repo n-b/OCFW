5aae18d16fdcf92bec5ed272e985c08ffcce872c
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2012-03-07 17:29:42 +0100

    Added experimental support for frameworks resources.

--- buildframeworks.sh
+++ buildframeworks.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+set -e
 
 # The project must be configured this way
 XCODEPROJ_NAME="Framework.xcodeproj"
 LIBRARY_TARGET_NAME="Library"
+RESOURCES_TARGET_NAME="Resources"
 PLATFORMS_FILENAME="platforms"
 
 ##
@@ -16,6 +18,12 @@ if [ ! -d $1 ]; then
 	exit -2
 fi
 
+##
+# options
+OUTDIR=${OUTDIR-`pwd`/Frameworks}
+echo "will create frameworks in ${OUTDIR}"
+
+##
 # move to target
 OLDWD=`pwd`
 cd $1
@@ -28,6 +36,10 @@ if [ ! -d ${XCODEPROJ_NAME} ]; then
 	exit -1
 fi
 PROJECT=${PROJECTS[0]}
+
+##
+# check that the project as a "Resources" target
+if [[ `xcodebuild -list ${XCODEPROJ_NAME}` == *${RESOURCES_TARGET_NAME}* ]]; then COPY_RESOURCES=true; fi
 
 
 ## 
@@ -52,6 +64,7 @@ echo "building ${LIBRARY} for platforms : ${PLATFORMS}"
 # internal constants
 SYMROOT="build"
 PUBLIC_HEADERS_FOLDER_PATH="Headers"
+LOGFILE=/tmp/$$.xcodebuild.log
 
 # Compile
 for SDK in $SDKS
@@ -65,31 +78,52 @@ do
         SDKROOT=${SDK}\
         PUBLIC_HEADERS_FOLDER_PATH=${PUBLIC_HEADERS_FOLDER_PATH}\
         SYMROOT=${SYMROOT}\
-		CONFIGURATION_BUILD_DIR=${SYMROOT}/${LIBRARY}.libraries/${SDK}\
+		CONFIGURATION_BUILD_DIR=${SYMROOT}/${LIBRARY}/${SDK}\
         ARCHS=\$VALID_ARCHS\
-		> /tmp/$$.xcodebuild.log # Store log away and display it in case of error
-	if [ $? -ne 0 ]
-		then
-		cat /tmp/$$.xcodebuild.log
-		rm /tmp/$$.xcodebuild.log	
-		exit 1
+			> ${LOGFILE}
+	if [ $? -ne 0 ]; then cat ${LOGFILE}; exit 1; fi
+	
+	# copy resources, if any
+	if [ $COPY_RESOURCES ]; then
+		echo " copying resources for ${SDK}"
+		xcodebuild build\
+	        -project ${XCODEPROJ_NAME}\
+	        -configuration Release\
+			-target ${RESOURCES_TARGET_NAME}\
+			PRODUCT_NAME=${RESOURCES_TARGET_NAME}\
+	        SDKROOT=${SDK}\
+			WRAPPER_EXTENSION=${RESOURCES_TARGET_NAME}\
+	        SYMROOT=${SYMROOT}\
+			CONFIGURATION_BUILD_DIR=${SYMROOT}/${LIBRARY}/${SDK}\
+				> ${LOGFILE}
+		if [ $? -ne 0 ]; then cat ${LOGFILE}; exit 1; fi
 	fi
-	rm /tmp/$$.xcodebuild.log	
 done
 
 # Make frameworks
 for PLATFORM in $PLATFORMS
 do
 	echo " making framework for ${PLATFORM}"
-    FRAMEWORK_PATH="${PLATFORM}/${LIBRARY}.framework"
-    mkdir -p "${FRAMEWORK_PATH}/Versions/A/Headers"
+    FRAMEWORK_PATH="${OUTDIR}/${PLATFORM}/${LIBRARY}.framework"
 
+	# create framework structure structure
+    mkdir -p "${FRAMEWORK_PATH}/Versions/A/Headers" "${FRAMEWORK_PATH}/Versions/A/Resources"
     ln -sfh "A" "${FRAMEWORK_PATH}/Versions/Current"
     ln -sfh "Versions/Current/Headers" "${FRAMEWORK_PATH}/Headers"
+    ln -sfh "Versions/Current/Resources" "${FRAMEWORK_PATH}/Resources"
     ln -sfh "Versions/Current/${LIBRARY}" "${FRAMEWORK_PATH}/${LIBRARY}"
 
-    cp -a `find ${SYMROOT}/${LIBRARY}.libraries/${PLATFORM}*/${PUBLIC_HEADERS_FOLDER_PATH}/` "${FRAMEWORK_PATH}/Versions/A/Headers"
-    lipo -create `find ${SYMROOT}/${LIBRARY}.libraries/${PLATFORM}*/lib${LIBRARY}.a` -output "${FRAMEWORK_PATH}/Versions/Current/${LIBRARY}"
+	# merge all corresponding binaries
+    lipo -create `find ${SYMROOT}/${LIBRARY}/${PLATFORM}*/lib${LIBRARY}.a` -output "${FRAMEWORK_PATH}/Versions/Current/${LIBRARY}"
+
+	# copy headers
+    cp -a `find ${SYMROOT}/${LIBRARY}/${PLATFORM}*/${PUBLIC_HEADERS_FOLDER_PATH}/` "${FRAMEWORK_PATH}/Versions/A/Headers"
+	
+	# copy resources
+	if [ $COPY_RESOURCES ]; then
+		cp -a `find ${SYMROOT}/${LIBRARY}/${PLATFORM}*/${RESOURCES_TARGET_NAME}.${RESOURCES_TARGET_NAME}/` "${FRAMEWORK_PATH}/Versions/A/Resources"
+	fi
+
 	echo "  ${FRAMEWORK_PATH}"
 done
 
